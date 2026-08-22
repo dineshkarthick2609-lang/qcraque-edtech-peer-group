2bc213c69823bfc52d03fb0881e61acc27c09fe0
dineshkarthick2609-lang <dineshkarthick2609@gmail.com>
2026-08-22 12:18:17 +0530

    Create types.ts

---
+++ frontend/src/types.ts
@@ -0,0 +1,11 @@
+export interface Student {
+  id: number;
+  student_id: string;
+  name: string;
+  email: string;
+  interests: string;
+  skills: string;
+  skill_level: string;
+  created_at?: string;
+  updated_at?: string;
+}
